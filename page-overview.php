c5d06c71554220b23e86b2a370643d71b6848bbf
bneeboy <46453074+bneeboy@users.noreply.github.com>
2020-08-14 03:42:29 -0700

    Add files via upload

--- page-overview.php
+++ page-overview.php
@@ -3,13 +3,30 @@
 Template name: Overview
 */
 get_header(); ?>
-
-
-	<main role="main">
+	<main>
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <h2 class="inner display-none">
+                </h2>
+				<?php the_content(); ?>
+				<!-- comments_template( '', true ); // Remove if you don't want comments -->
+				<br class="clear">
+				<?php edit_post_link(); ?>
+			</article>
+			<!-- /article -->
+		<?php endwhile; ?>
+		<?php else: ?>
+			<!-- article -->
+			<article>
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+			</article>
+			<!-- /article -->
+		<?php endif; ?>
 	</main>
 
-<aside class="sidebar">
-            <?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('info-sidebar')) ?>
+	<aside class="sidebar">
+		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('info-sidebar')) ?>
         </aside>
 
 <?php get_footer(); ?>
